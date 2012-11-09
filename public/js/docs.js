9cb96d8c7e58cc87942559d404f06e5b89389b43
Joe <joe@appfog.com>
2012-11-08 21:44:19 -0800

    collapsing works, needs styling

--- public/js/docs.js
+++ public/js/docs.js
@@ -1,13 +1,6 @@
 $(function() {
-
-	$('a.accordion-toggle').bind('click',function() {
-		if ( $(this).find('i.accordion-icon').hasClass('icon-chevron-right') ) {
-			$('.accordion-icon').removeClass('icon-chevron-down');
-			$('.accordion-icon').addClass('icon-chevron-right');
-			$(this).find('i').toggleClass('icon-chevron-down icon-chevron-right');
-		} else {
-			$(this).find('i').toggleClass('icon-chevron-down icon-chevron-right');
-		}
-	});
-
+    $('#collapse0').collapsible({
+        effect: "slide",
+        initialCollapse: true
+    });
 });
