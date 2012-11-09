7dd1dcd6546c94821a3e3b7623f89487d02792de
Joe Moon <joe@appfog.com>
2012-11-09 14:22:15 -0800

    moved routes to routes.js

--- app.js
+++ app.js
@@ -1,53 +1,11 @@
 var express = require('express'),
     app = express(),
-    mongoose = require('mongoose'),
-    db = mongoose.createConnection('localhost', 'afdocs'),
-    docsColl = new mongoose.Schema({
-        name: String,
-        title: String,
-        body: String
-    }),
-    menuColl = new mongoose.Schema({
-        menuArray: Array
-    }),
-    Doc = db.model('document', docsColl),
-    Menu = db.model('menu', menuColl);
-
-var menu = [];
-Menu.findOne({"title": "menu"}, function(err, menuArr){
-    menu = menuArr.menuArray;
-});
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    console.log('db conn');
-});
+    routes = require('./routes');
 
 app.set('views', __dirname + '/views');
 
 app.use('/static', express.static(__dirname + '/public'));
 app.use('/img', express.static(__dirname + '/public/img'));
-
-app.get('/*', function(req, res){
-    var pathArr = req.url.substring(1).split('/');
-    Doc.findOne({"path": req.url.substring(1)}, function(err, doc){
-        if(err){
-            console.log(err);
-        }else if(doc){
-            res.render('doc.jade', {
-                title: doc.title,
-                body: doc.body,
-                menu: menu,
-                path: req.url.substring(1),
-                pathArr: pathArr
-            });
-            console.log(req.url);
-            console.log(pathArr);
-        }else{
-            console.log(req.url + ' not found');
-            res.send(req.url + ' not found');
-        }
-    });
-});
+app.get('/*', routes.pages);
 
 app.listen(process.env.VCAP_APP_PORT || 3000);
---
+++ routes.js
@@ -0,0 +1,72 @@
+var mongoose = require('mongoose'),
+    db = mongoose.createConnection('localhost', 'afdocs'),
+    docsColl = new mongoose.Schema({
+        name: String,
+        title: String,
+        body: String,
+        redirect: String
+    }),
+    menuColl = new mongoose.Schema({
+        menuArray: Array
+    }),
+    Doc = db.model('document', docsColl),
+    Menu = db.model('menu', menuColl);
+
+var menu = [];
+
+Menu.findOne({"title": "menu"}, function(err, menuArr){
+    menu = menuArr.menuArray;
+});
+
+exports.pages = function(req, res){
+    var pathArr = req.url.substring(1).split('/');
+    if(req.url === '/'){
+        Doc.findOne({"path": "overview"}, function(err,doc){
+            if(err){
+                console.log(err);
+            }else if(doc){
+                res.render('doc.jade', {
+                    title: doc.title,
+                    body: doc.body,
+                    menu: menu,
+                    path: req.url.substring(1)
+                });
+            }else{
+                console.log(req.url + ' not found');
+                res.send(req.url + ' not found');
+            }
+        });
+    }else{
+        if(req.url[req.url.length - 1] === '/'){
+            res.redirect(301, req.url.substring(0, req.url.length - 1));
+        }else{
+            Doc.findOne({"path": req.url.substring(1)}, function(err, doc){
+                if(err){
+                    console.log(err);
+                }else if(doc){
+                    if(doc.redirect){
+                        res.redirect(301, doc.redirect);
+                    }else{
+                        res.render('doc.jade', {
+                            title: doc.title,
+                            body: doc.body,
+                            menu: menu,
+                            path: req.url.substring(1)
+                        });
+                    }
+                }else{
+                    Doc.findOne({"path": req.url.substring(1) + '/overview'}, function(err, doc){
+                        if(err){
+                            console.log(err);
+                        }else if(doc){
+                            res.redirect(301, req.url.substring(1) + '/overview');
+                        }else{
+                            console.log(req.url + ' not found');
+                            res.send(req.url + ' not found');
+                        }
+                    });
+                }
+            });
+        }
+    }
+}
