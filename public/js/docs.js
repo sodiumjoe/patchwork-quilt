41101940d646e47b4e6333b54d7058205588d732
Joe <joe@appfog.com>
2012-11-08 23:10:01 -0800

    menu toggles work

--- public/js/docs.js
+++ public/js/docs.js
@@ -4,3 +4,9 @@ $(function() {
         initialCollapse: true
     });
 });
+
+$(function() {
+	$('a.menu-parent').bind('click',function() {
+        $(this).find('i').toggleClass('icon-chevron-down icon-chevron-right');
+	});
+});
