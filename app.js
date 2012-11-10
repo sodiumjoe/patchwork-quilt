f78e69853a5788d30841ca8f039c4476fa39c91e
Joe Moon <joe@appfog.com>
2012-11-09 17:47:30 -0800

    remove local assets

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var express = require('express'),
 app.set('views', __dirname + '/views');
 
 app.use('/static', express.static(__dirname + '/public'));
-app.use('/img', express.static(__dirname + '/public/img'));
+//app.use('/img', express.static('https://s3.amazonaws.com/afdocs/assets/img'));
+app.get('/img/*', function(req, res){
+    res.redirect(301, 'https://s3.amazonaws.com/afdocs/assets' + req.url);
+});
 app.get('/search', routes.search);
 app.get('/*', routes.pages);
 
