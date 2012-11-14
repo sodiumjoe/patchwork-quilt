21384431a17d5fe20cb6b389ad01ce639182b9f2
Joe Moon <joe@appfog.com>
2012-11-14 15:54:32 -0800

    move routes to lib/ and config.js -> config.yml

--- config.js
+++
@@ -1,6 +0,0 @@
-var yaml = require('yamlparser'),
-    fs = require('fs'),
-    configYaml = fs.readFileSync('./config.yml', 'utf8'),
-    config = yaml.eval(configYaml);
-
-module.exports = config;
