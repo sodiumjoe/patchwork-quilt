11ea00dd847e876b1ce6e1483067b923845a903f
Joe Moon <joe@appfog.com>
2012-11-14 15:54:14 -0800

    basic routing

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 var express = require('express'),
-    config = require('./config.js'),
+    config = require('./lib/config.js'),
     app = express(),
-    routes = require('./routes');
-
-console.log(config);
+    routes = require('./lib/routes');
 
 app.set('views', __dirname + '/views');
 app.use('/static', express.static(__dirname + '/public'));
 
 app.get('/img/*', function(req, res){
-    res.redirect(301, 'https://s3.amazonaws.com/afdocs/assets' + req.url);
+    res.redirect(301, conf.assets + req.url);
 });
 app.get('/search', routes.search);
 app.get('/*', routes.pages);
 app.use(routes.errorHandler);
 
-app.listen(process.env.VCAP_APP_PORT || 3001);
+app.listen(process.env.VCAP_APP_PORT || 3000);
